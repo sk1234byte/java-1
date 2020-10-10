aa4d966f4dc0dd9d23b2ecfc4350b5a1ae7a28a5
sk1234byte <72659853+sk1234byte@users.noreply.github.com>
2020-10-10 22:28:26 +0530

    Update Main.java

--- Day 2/Main.java
+++ Day 2/Main.java
@@ -4,8 +4,8 @@ package com.employee2;
 public class Main {
 
   public static void main(String[] args) {
-   Employee emp1 = new Employee("Saurabh", 50, "Gurgaon");
-                Employee emp2 = new Employee("Kundan", 20, "Delhi");
+   Employee emp1 = new Employee("Saurabh", 23, "Gurgaon");
+                Employee emp2 = new Employee("shahid", 20, "Delhi");
         emp1.display();
                 emp2.display();
   }
